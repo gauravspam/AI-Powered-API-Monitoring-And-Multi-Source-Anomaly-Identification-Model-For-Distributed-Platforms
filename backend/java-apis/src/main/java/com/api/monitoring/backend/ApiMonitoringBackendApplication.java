920cc4e13e503b5966eb4b7da03fa4d009743976
Gaurav <gauravspamail@gmail.com>
2025-12-20 18:19:55 +0530

    Spring Initiated

---
+++ backend/java-apis/src/main/java/com/api/monitoring/backend/ApiMonitoringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.monitoring.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonitoringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonitoringBackendApplication.class, args);
+	}
+
+}
---
+++ backend/java-apis/src/test/java/com/api/monitoring/backend/ApiMonitoringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.monitoring.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonitoringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
